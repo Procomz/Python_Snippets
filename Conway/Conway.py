de52cebe13539d5a1afdbb991ff6291eec1149af
MG <mat.granjon@free.fr>
2019-02-27 23:08:29 +0100

    Conway OK + tested

--- Conway/Conway.py
+++ Conway/Conway.py
@@ -1,14 +1,31 @@
 # -*- coding: utf-8 -*-
+import itertools
 
 class Conway:
-    def __init__(self, startStr):
-        self.startStr = startStr
+    def __init__(self, seed):
+        self.seed = seed
+        self.current = self.seed
         
-    def compute(self):
-        for char in self.startStr:
-            print(char)
+    def computeNext(self):
+        nextstr = ""
+        items = [list(g) for k,g in itertools.groupby(self.current)]
+        
+        for item in items:
+            nextstr = nextstr + str(len(item)) + item[0]
+            
+        self.current = nextstr
+        return self.current
+        
+    def printCurrent(self):
+        print(self.current)
     
+    def getCurrent(self):
+        return self.current
+        
 if __name__ == "__main__":
     print("debug")
-    c = Conway("1")
-    c.compute()
+    c = Conway('1')
+    c.printCurrent()
+    for i in range(10):
+        print(c.computeNext())
+    
--- Conway/Conway_tests.py
+++ Conway/Conway_tests.py
@@ -1,6 +1,21 @@
 # -*- coding: utf-8 -*-
 import sys
+import unittest
+from Conway import Conway
 
+class ConwayTest(unittest.TestCase):
+    def test_Conway1(self):
+        c = Conway('1')
+        self.assertEqual(c.getCurrent(), '1')
+        self.assertEqual(c.computeNext(), '11')
+        self.assertEqual(c.computeNext(), '21')
+        self.assertEqual(c.computeNext(), '1211')
+        self.assertEqual(c.computeNext(), '111221')
+        self.assertEqual(c.computeNext(), '312211')
+        self.assertEqual(c.computeNext(), '13112221')
+        
+        
 if __name__ == "__main__":
-    print("testing")
-    print(sys.version)
+    print("Testing Conway")
+    print("Running on : " + sys.version)
+    unittest.main()
